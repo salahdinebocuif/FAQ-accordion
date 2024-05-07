7905862bfa5401845b178c55d48d8964fb52a927
salah <salahsalahdine323@gmail.com>
2024-05-07 17:43:42 +0100

    modified file

---
+++ new.js
@@ -0,0 +1,60 @@
+document.getElementById("button1").onclick=function(){
+  var div1=document.getElementById("a1");
+  var icon1=document.getElementById("plus1");
+  var icon2=document.getElementById("mins1");
+  if(div1.style.display=="none"){
+    div1.style.display="block";
+    icon1.style.display="none"
+    icon2.style.display="block";
+  }
+  else{
+    div1.style.display="none";
+    icon1.style.display="block"
+    icon2.style.display="none";
+  }
+}
+document.getElementById("button2").onclick=function(){
+  var div1=document.getElementById("a2");
+  var icon1=document.getElementById("plus2");
+  var icon2=document.getElementById("mins2");
+  if(div1.style.display=="none"){
+    div1.style.display="block";
+    icon1.style.display="none"
+    icon2.style.display="block";
+  }
+  else{
+    div1.style.display="none";
+    icon1.style.display="block"
+    icon2.style.display="none";
+  }
+}
+document.getElementById("button3").onclick=function(){
+  var div1=document.getElementById("a3");
+  var icon1=document.getElementById("plus3");
+  var icon2=document.getElementById("mins3");
+  if(div1.style.display=="none"){
+    div1.style.display="block";
+    icon1.style.display="none"
+    icon2.style.display="block";
+  }
+  else{
+    div1.style.display="none";
+    icon1.style.display="block"
+    icon2.style.display="none";
+  }
+}
+document.getElementById("button4").onclick=function(){
+  var div1=document.getElementById("a4");
+  var icon1=document.getElementById("plus4");
+  var icon2=document.getElementById("mins4");
+  if(div1.style.display=="none"){
+    div1.style.display="block";
+    icon1.style.display="none"
+    icon2.style.display="block";
+  }
+  else{
+    div1.style.display="none";
+    icon1.style.display="block"
+    icon2.style.display="none";
+  }
+}
